3d50fef5632933d39a7a05dc3a92cd2257eab222
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-11-07 17:56:18 +0400

    account sdas

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -44,7 +44,7 @@ export default function GTCRegisterWithDesign() {
                 <CelebrationPanel values={submittedName} />
               ) : (
                 <div className="">
-                  <div className="flex items-center justify-center w-full gap-4 md:mb-20">
+                  <div className="flex items-center justify-center w-full gap-4  mb-5 md:mb-20">
                     <Image
                       src="/new-logo.webp"
                       width={260}
